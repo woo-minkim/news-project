991b0c887b278a6119674d4a1dc9126663825352
woo-minkim <kmwoo2314@kakao.com>
2024-11-20 02:50:42 +0900

    [docs]

--- pipeline.py
+++ pipeline.py
@@ -1,3 +1,9 @@
+# pipeliine.py 역할 
+# 1. 데이터 수집: GDELT API를 사용하여 지정된 기업과 관련된 뉴스 기사를 수집.
+# 2. 데이터 추출: newspaper 라이브러리를 사용하여 기사 본문과 제목을 추출.
+# 3. 데이터 분석: OpenAI GPT 모델을 사용하여 감성 분석을 수행.
+# 4. 데이터 저장: 분석된 데이터를 MongoDB에 저장.
+# -> 이후 Streamlit을 사용하여 저장된 데이터를 시각화할 수 있음.
 from prompt_template import prompt_template
 
 from openai import OpenAI
@@ -9,24 +15,25 @@ import datetime
 
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 openai_client = OpenAI(api_key=OPENAI_API_KEY)
 model = "gpt-4o-mini"
-gd = GdeltDoc()
-mongo_client = MongoClient(host="localhost", port=27017)
-db = mongo_client["NewsProject"]
-collection = db["NewsAnalysisDate"]
+gd = GdeltDoc() #GdeltDoc 클래스의 인스턴스 생성, Gdelt 데이터베이스에 접근 가능
+mongo_client = MongoClient(host="localhost", port=27017) #MongoDB 서버와 연결 
+db = mongo_client["NewsProject"] #"NewsProject" 데이터베이스에 접근
+collection = db["NewsAnalysisDate"] #"NewsAnalysisDate" 컬렉션에 접근, 이곳에 분석된 뉴스 기사의 데이터를 저장
 
 
-# chatGPT 답변 생성 함수
+# query를 기반으로 chatGPT 답변 생성 함수
 def chatgpt_generate(query):
-  messages = [{
-    "role": "system", 
+  messages = [{ #GPT모델에게 전달할 메시지 리스트
+    "role": "system", #모델의 역할과 성격 설정
     "content": "You are a helpful assistant."
   }, 
-  {
+  { #사용자로부터 받은 질문
     "role": "user",
     "content": query
   }]
@@ -44,26 +51,27 @@ def get_url(keyword):
     domain = "nytimes.com",
     country = "US",
   )  
-  articles = gd.article_search(f)
+  articles = gd.article_search(f) # (Pandas) DataFrame 형태로 반환
   return articles
 
 # 뉴스 기사 URL을 이용하여 뉴스 기사 내용 가져오기
 def get_article(df):
-  urls = df["url"]
-  titles = df["title"]
-  texts = []
+  urls = df["url"] # 기사 URL을 저장할 리스트
+  titles = df["title"] # 기사 제목을 저장할 리스트
+  texts = [] # 기사 본문을 저장할 리스트
   for url in urls:
-    article = Article(url)
-    article.download()
-    article.parse()
-    texts.append(article.text)
+    article = Article(url) # newspaper3k 라이브러리의 Article 클래스로 기사 객체 생성
+    article.download() #지정된 URL에서 기사의 HTML 데이터 다운로드
+    article.parse() #다운로드한 HTML을 파싱하여 기사의 본문을 추출
+    texts.append(article.text) #추출한 기사 본문을 리스트에 추가
   return texts, titles
 
+# 지정된 기업 리스트(orgs)에 대해 GDELT API를 사용해 관련 뉴스를 수집하고 GPT 모델을 통해 분석한 후 결과를 MOngoDB에 저장
 def analysis():
   orgs = ["microsoft", "apple"]  # 분석할 기업 이름을 저장한 리스트
   
   for org in orgs:  
-    df = get_url(org)  # `get_url` 함수를 호출하여 해당 기업과 관련된 데이터를 가져옴
+    df = get_url(org)  # `get_url` 함수를 호출하여 해당 기업과 관련된 메타데이터를 가져옴
     dates = df["seendate"]  # 데이터프레임에서 기사 날짜 정보를 가져옴
     texts, titles = get_article(df)  # `get_article` 함수로 기사 본문과 제목을 가져옴
     
@@ -85,3 +93,7 @@ def analysis():
   return  # 함수 종료
 
 analysis()
+
+# Sentiment Scoring? 
+# 텍스트 데이터에서 긍정적, 부정적, 중립적 감정을 분석하고,
+# 각 감정에 대한 점수를 부여하는 과정 
--- visualization.py
+++ visualization.py
@@ -1,3 +1,6 @@
+# Streamlit Application의 역할 
+# pipeline.py에서 저장된 데이터를 조회 및 시각화하는 역할을 함
+
 import streamlit as st  # Streamlit 라이브러리 import (웹 애플리케이션 생성용)
 import pandas as pd  # pandas 라이브러리 import (데이터프레임 및 데이터 처리용)
 from pymongo import MongoClient  # MongoDB와 연결하기 위한 라이브러리 import
@@ -26,8 +29,9 @@ st.title("기업별 날짜에 따른 감성 지수 변화")
 # 사용자로부터 선택할 기업 옵션을 드롭다운으로 제공
 organization = st.selectbox("기업을 선택하세요.", ['Microsoft', 'Google'])  
 
-# 선택된 기업의 데이터만 필터링
+# 선택된 기업의 데이터만 필터링 / 날짜를 인덱스로 설정
 selected_df = df.loc[df['organization'] == organization].set_index('seendate')  
 
 # 긍정(positive), 부정(negative), 중립(neutral) 감성 지수를 선 그래프로 표시
 st.line_chart(selected_df[['positive', 'negative', 'neutral']])  
+
